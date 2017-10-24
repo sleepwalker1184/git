bb4a6a7806c4aa2493aa2c2f5296a89343e38ac4
sleepwalker1184 <zhang4312106@gmail.com>
2017-10-24 19:38:38 +0800

    inital exp 3

---
+++ demo2/demo2/exp.cpp
@@ -0,0 +1,9 @@
+//
+//  exp.cpp
+//  demo2
+//
+//  Created by zhang Alex on 2017/10/23.
+//  Copyright © 2017年 zhang Alex. All rights reserved.
+//
+
+#include "exp.hpp"
---
+++ demo2/demo2/exp.hpp
@@ -0,0 +1,18 @@
+//
+//  exp.hpp
+//  demo2
+//
+//  Created by zhang Alex on 2017/10/23.
+//  Copyright © 2017年 zhang Alex. All rights reserved.
+//
+
+#ifndef exp_hpp
+#define exp_hpp
+#include "Base.hpp"
+
+class exp:public Base
+{
+    
+};
+
+#endif /* exp_hpp */
