ad397953c86e6125faa8f188cfbdcacd690d3fbf
sleepwalker1184 <zhang4312106@gmail.com>
2017-11-29 16:06:54 +0800

    explain

---
+++ PanCake/PanCake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PanCake
+//
+//  Created by zhang Alex on 2017/11/29.
+//  Copyright © 2017年 zhang Alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
